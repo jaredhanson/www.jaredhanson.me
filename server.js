1e13afc60ef6ad7e2c9e269f6a7b9381e7162b3b
Jared Hanson <jaredhanson@gmail.com>
2022-02-18 15:43:07 -0800

    Serve dist.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
