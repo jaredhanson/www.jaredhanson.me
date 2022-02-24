d6e4faad33088ec836388554b1720f4a96520b35
Jared Hanson <jaredhanson@gmail.com>
2022-02-24 11:30:37 -0800

    Remove dist directory from static server.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
